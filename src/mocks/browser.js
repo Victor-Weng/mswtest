5dab4177ed8f5e98b785a846b8e48c4031f55166
Victor-Weng <VictorWengVTW@gmail.com>
2025-02-09 18:52:00 -0500

    fixed to npx msw init command

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,7 +1,7 @@
 import { setupWorker } from "msw/browser";
 import { authHandlers } from "./authHandlers";
 import { accessControlHandlers } from "./accessControlHandlers";
-//npx msw init public
+//npx msw init .
 
 const handlers = [...authHandlers, ...accessControlHandlers];
 
